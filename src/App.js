8247f04c8eb1eb286f1e9d5ecfe149c4304b3478
Nathan Merrill <nmerrill5@gmail.com>
2022-06-01 08:37:06 -0600

    Parse data from csv better

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import Papa from 'papaparse';
 import { PatientDataContext } from './patient_data_context';
 import { HashRouter, NavLink, Outlet, Route, Routes, useNavigate } from 'react-router-dom';
 
+// Info about how form fields should be parsed. Any fields not included here will be left as strings
+const submission_info = {
+  // UTC with date and time
+  dateTime: ['Submission Date'],
+  // For date-only strings, parsed as UTC midnight
+  date: [],
+  // Multiple choice questions will be parsed to an array
+  multipleChoice: ['Multiple Choice Question'],
+  int: ['Number Question'],
+  float: [],
+};
+
 const Intake = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [, setPatientData] = useContext(PatientDataContext);
@@ -20,11 +32,38 @@ const Intake = () => {
     Papa.parse(selectedFile, {
       header: true,
       complete: (results) => {
-        setPatientData(results);
+        console.log(results.data);
+        const parsedData = parseData(results.data);
+        console.log(parsedData);
+        setPatientData(parsedData);
         navigate('/');
       },
     });
   };
+
+  const parseData = (data) => {
+    const parsedData = data.map((submission) => {
+      const result = submission;
+      submission_info.dateTime.map((field) => (result[field] = parseDateTime(result[field])));
+      submission_info.date.map((field) => (result[field] = parseDate(result[field])));
+      submission_info.multipleChoice.map((field) => (result[field] = result[field].split('\n')));
+      submission_info.int.map((field) => (result[field] = parseInt(result[field])));
+      submission_info.float.map((field) => (result[field] = parseFloat(result[field])));
+      return result;
+    });
+    return parsedData;
+  };
+
+  // https://stackoverflow.com/questions/5619202/parsing-a-string-to-a-date-in-javascript
+  const parseDateTime = (rawDateTimeString) => {
+    const [dateString, timeString] = rawDateTimeString.split(' ');
+    const paddedTimeString = timeString.length === 8 ? timeString : '0' + timeString;
+    const preppedDateTimeString = dateString + 'T' + paddedTimeString + 'Z';
+    console.log(preppedDateTimeString);
+    return new Date(preppedDateTimeString);
+  };
+
+  const parseDate = (rawDateString) => rawDateString;
 
   return (
     <>
@@ -47,7 +86,13 @@ const Home = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>This is the homepage</div>;
+  return (
+    <div>
+      <div>Welcome to the app!</div>
+      <div>Yesterday's new patients:</div>
+      {}
+    </div>
+  );
 };
 
 const links = [
